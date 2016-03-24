f29c8ae073d72106f5248521b31334c05e2eb8b7
reubencapio <reubencapio@gmail.com>
2016-03-24 15:18:21 +0800

    Update stochastic_hill_climbing.cpp

--- stochastic_hill_climbing.cpp
+++ stochastic_hill_climbing.cpp
@@ -22,18 +22,17 @@ int onemax(const std::vector<std::string> &candidate_vector)
 }
 
 //change random values in the vector then sum them up to see if it would result to a more optimized value set
-void random_neighbor(std::vector<std::string> &candidate_vector, std::vector<std::string> &mutant, const int num_bits)
+void random_neighbor(const std::vector<std::string> &candidate_vector, std::vector<std::string> &mutant)
 {
 	mutant = candidate_vector;
 	std::random_device rd;
 	std::default_random_engine generator(rd());
 	//0.1 and 0.9 used to generate random numbers just between those 2 decimals
 	//based on c++11 style
-	std::uniform_int_distribution<int> distribution(0, num_bits - 1);
+	std::uniform_int_distribution<int> distribution(0, candidate_vector.size() - 1);
 	int pos = distribution(generator);
 	std::cout << "pos: " << pos << "\n";
 	mutant[pos] = (mutant[pos] == "1") ? "0" : "1";
-	candidate_vector[pos] = mutant[pos];
 }
 
 
@@ -43,7 +42,7 @@ void random_bitstring(const int num_bits, std::vector<std::string> &candidate_ve
 	for (int i = 0; i < num_bits; i++) {
 		std::random_device rd;
 		std::default_random_engine generator(rd());
-		//0.1 and 0.9 used to generate random numbers just between those 2 decimals
+		//0.0 and 1.0 used to generate random numbers just between those 2 numbers
 		//based on c++11 style
 		std::uniform_real_distribution<double> distribution(0.0, 1.0);
 		double random_num = distribution(generator);
@@ -57,8 +56,8 @@ double search(const int max_iterations, const int num_bits)
 {
 	std::vector<std::string> candidate_vector;
 	random_bitstring(num_bits, candidate_vector);
-	std::cout << "candidate_vector 1: ";
-	for(auto mv :candidate_vector) {
+	std::cout << "candidate_vector start: ";
+	for (auto mv : candidate_vector) {
 		std::cout << mv << " ";
 	}
 	std::cout << "\n";
@@ -67,26 +66,31 @@ double search(const int max_iterations, const int num_bits)
 	for (int i = 0; i < max_iterations; i++) {
 		const std::vector<std::vector<int>> neighbor;
 		std::vector<std::string> neighbor_vector;
-		random_neighbor(candidate_vector, neighbor_vector, num_bits);
-		std::cout << "candidate vec 2: ";
-		for (auto mv : candidate_vector) {
-			std::cout << mv << " ";
-		}
-		std::cout << "\n";
+		random_neighbor(candidate_vector, neighbor_vector);
 		int neighbor_cost = onemax(neighbor_vector);
 		std::cout << "neighbor_vector: ";
 		for (auto mv : neighbor_vector) {
-			std::cout <<  mv << " ";
+			std::cout << mv << " ";
 		}
 		std::cout << "\n";
-
+		if (neighbor_cost >= candidate_cost) {
+			candidate_cost = neighbor_cost;
+			candidate_vector.assign(neighbor_vector.begin(), neighbor_vector.end());
+		}
 		std::cout << "neighbor_cost: " << neighbor_cost << "\n";
-		candidate_cost = neighbor_cost >= candidate_cost ? neighbor_cost : candidate_cost;
+		for (auto mv : neighbor_vector) {
+			std::cout << mv << " ";
+		}
 		std::cout << "iteration : " << i << "  best= " << candidate_cost << "\n";
 		if (candidate_cost == num_bits) {
 			break;
 		}
 	}
+	std::cout << "candidate_vector end: ";
+	for (auto mv : candidate_vector) {
+		std::cout << mv << " ";
+	}
+	std::cout << "\n";
 	return candidate_cost;
 }
 
