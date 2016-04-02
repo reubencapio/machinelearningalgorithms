4579fe9e2a897cd640b7c9e7ed8040ba051a1bc4
reubencapio <reubencapio@gmail.com>
2016-04-02 17:27:18 +0800

    Update neural_perceptron.cpp

--- neural_perceptron.cpp
+++ neural_perceptron.cpp
@@ -3,98 +3,87 @@
 #include <stdlib.h> 
 #include <vector>
 #include <map>
-
+#include <random>
+#include <functional>
 
 //return a vector(with size same as that of problem_size) with random numbers generated using search_space members as basis
-void random_vector(std::vector<std::vector<double>> &mysearch_space, std::vector<double> &mynew_vec, int problem_size)
+void random_vector(const std::vector<std::vector<double>> &minmax, std::vector<double> &array_new)
 {
-   for(int i = 0; i < problem_size; i++){
-	mynew_vec.push_back(mysearch_space[i][0] + ( (mysearch_space[i][1] - mysearch_space[i][0]) * rand() ) );
-   }
+	for (int i = 0; i < (int)minmax.size(); i++){
+		std::cout << rand() << "\n";
+		std::default_random_engine generator;
+		std::uniform_real_distribution<double> distribution(0.000000000000001, 0.999999999999999);
+		auto rand = std::bind(distribution, generator);
+		array_new.push_back(minmax[i][0] + ((minmax[i][1] - minmax[i][0]) * rand()));
+	}
 }
 
 
-def initialize_weights(problem_size)
-  minmax = Array.new(problem_size + 1) {[-1.0,1.0]}
-  return random_vector(minmax)
-end
+void initialize_weights(int problem_size, std::vector<std::vector<double>> &minmax, std::vector<double> &array_new)
+{
+	std::vector<double> in_minmax = { -1.0, 1.0 };
+	for (int i = 0; i < problem_size + 1; i++){
+		minmax.push_back(in_minmax);
+	}
+	random_vector(minmax, array_new);
+}
 
-def update_weights(num_inputs, weights, input, out_exp, out_act, l_rate)
-  num_inputs.times do |i|
-    weights[i] += l_rate * (out_exp - out_act) * input[i]
-  end
-  weights[num_inputs] += l_rate * (out_exp - out_act) * 1.0
-end
+void update_weights(int num_inputs, std::vector<double> &weights, std::vector<double> &input, double out_exp,
+	double out_act, double l_rate)
+{
+	for (int i = 0; i < num_inputs; i++){
+		weights[i] += l_rate * (out_exp - out_act) * input[i];
+	}
+	weights[num_inputs] += l_rate * (out_exp - out_act) * 1.0;
+}
 
-def activate(weights, vector)
-  sum = weights[weights.size-1] * 1.0
-  vector.each_with_index do |input, i|
-    sum += weights[i] * input
-  end
-  return sum
-end
+double activate(std::vector<double> &weights, std::vector<double> &a_vector)
+{
+	double sum = weights[weights.size() - 1] * 1.0;
+	for (int i = 0; i < (int)a_vector.size(); i++){
+		sum += weights[i] * a_vector[i];
+	}
+	return sum;
+}
 
-def transfer(activation)
-  return (activation >= 0) ? 1.0 : 0.0
-end
 
-def get_output(weights, vector)
-  activation = activate(weights, vector)
-  return transfer(activation)
-end
 
-def train_weights(weights, domain, num_inputs, iterations, lrate)
-  iterations.times do |epoch|
-    error = 0.0
-    domain.each do |pattern|
-      input = Array.new(num_inputs) {|k| pattern[k].to_f}
-      output = get_output(weights, input)
-      expected = pattern.last.to_f
-      error += (output - expected).abs
-      update_weights(num_inputs, weights, input, expected, output, lrate)
-    end
-    puts "> epoch=#{epoch}, error=#{error}"
-  end
-end
+double transfer(double activation)
+{
+	return (activation >= 0) ? 1.0 : 0.0;
+}
 
-def test_weights(weights, domain, num_inputs)
-  correct = 0
-  domain.each do |pattern|
-    input_vector = Array.new(num_inputs) {|k| pattern[k].to_f}
-    output = get_output(weights, input_vector)
-    correct += 1 if output.round == pattern.last
-  end
-  puts "Finished test with a score of #{correct}/#{domain.size}"
-  return correct
-end
+double get_output(std::vector<double> &weights, std::vector<double> &a_vector)
+{
+	double activation = activate(weights, a_vector);
+	return transfer(activation);
+}
 
-def execute(domain, num_inputs, iterations, learning_rate)
-  weights = initialize_weights(num_inputs)
-  train_weights(weights, domain, num_inputs, iterations, learning_rate)
-  test_weights(weights, domain, num_inputs)
-  return weights
-end
-
-if __FILE__ == $0
-  # problem configuration
-  or_problem = [[0,0,0], [0,1,1], [1,0,1], [1,1,1]]
-  inputs = 2
-  # algorithm configuration
-  iterations = 20
-  learning_rate = 0.1
-  # execute the algorithm
-  execute(or_problem, inputs, iterations, learning_rate)
-end
+void train_weights(std::vector<double> &weights, std::vector<std::vector<double>> &domain, const int num_inputs, 
+	              const int iterations, const double lrate)
+{
+	for (int i = 0; i < iterations; i++){
+		double error = 0.0;
+		for (auto pattern : domain){
+			std::vector<double> input;
+			for (int j = 0; j < num_inputs; j++){
+				input.push_back(pattern[j]);
+			}
+			double output = get_output(weights, input);
+			double expected = pattern.back();
+			error += output - expected;
+			update_weights(num_inputs, weights, input, expected, output, lrate);
+		}
+	}
+}
 
 
 int main()
 {
-  //problem configuration
-  std::vector<std::vector<double>> or_problem { {0,0,0}, {0,1,1}, {1,0,1}, {1,1,1}};
-  const int inputs = 2;
-  //algorithm configuration
-  const int iterations = 20;
-  const double learning_rate = 0.1;
-  //execute the algorithm
-  execute(or_problem, inputs, iterations, learning_rate);
+	const int inputs = 2;
+	std::vector<std::vector<double>> minmax;
+	std::vector<double> array_new;
+	double v2 = ((double)rand()) / ((double)RAND_MAX) * 0.9999999999999999 + 0.000000000000001;
+	std::cout << "v2 = " << v2 << "\n";
+	initialize_weights(inputs, minmax, array_new);
 }
