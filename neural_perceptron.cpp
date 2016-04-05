a70120ee00f220d30ee0ae945f1af1d82ce59116
reubencapio <reubencapio@gmail.com>
2016-04-05 23:40:14 +0800

    Update neural_perceptron.cpp

--- neural_perceptron.cpp
+++ neural_perceptron.cpp
@@ -6,20 +6,23 @@
 #include <random>
 #include <functional>
 
-//return a vector(with size same as that of problem_size) with random numbers generated using search_space members as basis
+//return a vector(with size same as that of problem_size) with random numbers generated using minmax members as basis
 void random_vector(const std::vector<std::vector<double>> &minmax, std::vector<double> &array_new)
 {
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(0.000000000000001, 0.999999999999999);
+	auto rand = std::bind(distribution, generator);
 	for (int i = 0; i < (int)minmax.size(); i++){
-		std::cout << rand() << "\n";
-		std::default_random_engine generator;
-		std::uniform_real_distribution<double> distribution(0.000000000000001, 0.999999999999999);
-		auto rand = std::bind(distribution, generator);
+		//std::cout << "minmax[i][1] : " << minmax[i][1] << " \n" ;
+		//std::cout << "minmax[i][0] : " << minmax[i][0] << " \n";
+		//std::cout << "rand() : " << rand() << " \n";
 		array_new.push_back(minmax[i][0] + ((minmax[i][1] - minmax[i][0]) * rand()));
 	}
 }
 
 
-void initialize_weights(int problem_size, std::vector<double> &array_new)
+//intialize a 2 dimension vector with -1.0 and 1 then feed it to random_vector() function
+void initialize_weights(const int problem_size, std::vector<double> &array_new)
 {
 	std::vector<std::vector<double>> minmax;
 	std::vector<double> in_minmax = { -1.0, 1.0 };
@@ -29,16 +32,30 @@ void initialize_weights(int problem_size, std::vector<double> &array_new)
 	random_vector(minmax, array_new);
 }
 
-void update_weights(int num_inputs, std::vector<double> &weights, std::vector<double> &input, double out_exp,
-	double out_act, double l_rate)
+//update wights based on input vector, expected value, output value and learning rate, called from train_weights
+void update_weights(const int num_inputs, std::vector<double> &weights, const std::vector<double> &input, const double out_exp,
+	                const double out_act, const double l_rate)
 {
 	for (int i = 0; i < num_inputs; i++){
+		std::cout << "l_rate: " << l_rate << ",  " << "out_exp: " << out_exp << ",  " << "out_act: " << out_act << ",  " << "input[i]: "
+			      << input[i] << " \n";
+		double temp = weights[i] + l_rate * (out_exp - out_act) * input[i];
+		std::cout << "temp: " << temp << " \n";
+		//bug? wights[0] does not seem to change value
 		weights[i] += l_rate * (out_exp - out_act) * input[i];
+		std::cout << "i: " << i << ",  weights[i]: " << weights[i] << "\n";
 	}
 	weights[num_inputs] += l_rate * (out_exp - out_act) * 1.0;
+	std::cout << "num_inputs: " << num_inputs << ",  weights[num_inputs]: " << weights[num_inputs] << "\n";
+	std::cout << "weights: ";
+	for (auto w : weights){
+		std::cout << w << " ";
+	}
+	std::cout << "\n";
 }
 
-double activate(std::vector<double> &weights, std::vector<double> &a_vector)
+//sum up product of weight and 2 member from domain 2d vector(a_vector)
+double activate(const std::vector<double> &weights, const std::vector<double> &a_vector)
 {
 	double sum = weights[weights.size() - 1] * 1.0;
 	for (int i = 0; i < (int)a_vector.size(); i++){
@@ -48,38 +65,56 @@ double activate(std::vector<double> &weights, std::vector<double> &a_vector)
 }
 
 
-
-double transfer(double activation)
+//return 1 or 0 based on value of activation
+double transfer(const double activation)
 {
+	//std::cout << "activation: " << activation << "\n";
 	return (activation >= 0) ? 1.0 : 0.0;
 }
 
-double get_output(std::vector<double> &weights, std::vector<double> &a_vector)
+//sum up product of weight and 2 member from domain 2d vector(a_vector)
+//then return 1 or 0 based on value of activation
+double get_output(const std::vector<double> &weights, const  std::vector<double> &a_vector)
 {
 	double activation = activate(weights, a_vector);
 	return transfer(activation);
 }
 
-void train_weights(std::vector<double> &weights, std::vector<std::vector<double>> &domain, const int num_inputs,
+void train_weights(std::vector<double> &weights, const std::vector<std::vector<double>> &domain, const int num_inputs,
 	const int iterations, const double lrate)
 {
+	//iterations is 20
 	for (int i = 0; i < iterations; i++){
 		double error = 0.0;
+		//domain is a 2 dimensional vector
+		//pattern contains 3 members
 		for (auto pattern : domain){
 			std::vector<double> input;
+			//num_inputs is 2
+			//get first 2 members of pattern array then push them inside input vector
 			for (int j = 0; j < num_inputs; j++){
 				input.push_back(pattern[j]);
 			}
+			//sum up product of weight and 2 member from domain 2d vector(a_vector)
+			//then return 1 or 0 based on value of activation
 			double output = get_output(weights, input);
+			//get last member of pattern array
 			double expected = pattern.back();
 			error += abs(output - expected);
+			//update wights based on input vector, expected value, output value and learning rate
 			update_weights(num_inputs, weights, input, expected, output, lrate);
+			/*std::cout << "weights: ";
+			for (auto w:weights){
+				std::cout << w << " ";
+			}
+			std::cout << "\n";*/
 		}
 		std::cout << "epoch: " << i << ", error: " << error << "\n";
 	}
 }
 
-int test_weights(std::vector<double> &weights, std::vector<std::vector<double>> domain, const int num_inputs)
+//test weights after training them
+int test_weights(const std::vector<double> &weights, const std::vector<std::vector<double>> domain, const int num_inputs)
 {
 	int correct = 0;
 	for (auto pattern : domain){
@@ -97,28 +132,34 @@ int test_weights(std::vector<double> &weights, std::vector<std::vector<double>> 
 	return correct;
 }
 
-void execute(std::vector<std::vector<double>> domain, const int num_inputs, const int iterations, const double learning_rate)
+void execute(const std::vector<std::vector<double>> domain, const int num_inputs, const int iterations, const double learning_rate)
 {
 	std::vector<double> weights;
+	//intialize a 2 dimension vector with -1.0 and 1 then feed it to random_vector() function, return it as weights
 	initialize_weights(num_inputs, weights);
+	/*std::cout << "weights: ";
+	for (auto m : weights){
+		std::cout << m << " ";
+	}
+	std::cout << "\n";*/
 	train_weights(weights, domain, num_inputs, iterations, learning_rate);
 	test_weights(weights, domain, num_inputs);
-	std::cout << "weights == ";
-	for (auto w:weights){
+	/*std::cout << "weights == ";
+	for (auto w : weights){
 		std::cout << w << " ";
 	}
-	std::cout << "\n";
+	std::cout << "\n";*/
 }
 
 
 int main()
 {
 	//problem configuration
-	std::vector<std::vector<double>> or_problem = { { 0, 0, 0 }, { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+	const std::vector<std::vector<double>> or_problem = { { 0, 0, 0 }, { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
 	const int inputs = 2;
 	//algorithm configuration
 	const int iterations = 20;
-	double learning_rate = 0.1;
+	const double learning_rate = 0.1;
 	//execute the algorithm
 	execute(or_problem, inputs, iterations, learning_rate);
 }
