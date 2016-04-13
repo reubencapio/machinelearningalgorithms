f5501a45b79190871eb9f2bb601a57930371ed5b
reubencapio <reubencapio@gmail.com>
2016-04-13 10:11:00 +0800

    Update evolutionary_genetic_algorithm.cpp

--- evolutionary_genetic_algorithm.cpp
+++ evolutionary_genetic_algorithm.cpp
@@ -28,11 +28,11 @@ void random_bitstring(int num_bits, std::vector<std::string> &bitstring)
 }
 
 
-int binary_tournament(const std::vector<int> &population_fitness;)
+int binary_tournament(const std::vector<int> &population_fitness)
 {
 	std::random_device rd;
 	std::default_random_engine generator(rd());
-	std::uniform_int_distribution<int> distribution(pop.size());
+	std::uniform_int_distribution<int> distribution(population_fitness.size());
 	auto rand = std::bind(distribution, generator);
 	int i = rand();
 	int j = rand();
@@ -42,59 +42,82 @@ int binary_tournament(const std::vector<int> &population_fitness;)
 			break;
 		}
 	}
-	return (population_fitness[i] > population_fitness[j]) ? population_fitness[i] : population_fitness[j]
+	return (population_fitness[i] > population_fitness[j]) ? population_fitness[i] : population_fitness[j];
 }
 
-def point_mutation(bitstring, rate = 1.0 / bitstring.size)
-child = ""
-bitstring.size.times do | i |
-bit = bitstring[i].chr
-child << ((rand()<rate) ? ((bit == '1') ? "0" : "1") : bit)
-	end
-	return child
-	end
+void point_mutation(const std::vector<std::string> &bitstring, std::vector<std::string> &child)
+{
+	double rate = 1.0 / bitstring.size();
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::uniform_real_distribution<double> distribution(0.000000000000000001, 0.99999999999999999);
+	auto rand = std::bind(distribution, generator);
+	for (int i = 0; i < bitstring.size(); i++) {
+		std::string bit = bitstring[i];
+		child.push_back((rand()<rate) ? ((bit == "1") ? "0" : "1") : bit)
+	}
+}
 
-	def crossover(parent1, parent2, rate)
-	return "" + parent1 if rand() >= rate
-	point = 1 + rand(parent1.size - 2)
-	return parent1[0...point] + parent2[point...(parent1.size)]
-	end
+void crossover(const std::vector<std::string> &parent1, const std::vector<std::string> &parent2, const double rate, std::vector<std::string> &mixed_parent)
+{
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::uniform_real_distribution<double> distribution(0.000000000000000001, 0.99999999999999999);
+	auto rand = std::bind(distribution, generator);
+	if(rand() >= rate){
+		return parent1;
+	} else {
+		std::random_device rd2;
+		std::default_random_engine generator(rd2());
+		std::uniform_real_distribution<double> distribution2(parent1.size() - 2);
+		auto rand = std::bind(distribution, generator);
+		
+		int point = 1 + rand();
+		for(int i = 0; i < point; i++){
+			mixed_parent.push_back(parent1[i]);
+		}
+		
+		for(int i = point; i < parent1.size(); i++){
+			mixed_parent.push_back(parent1[i]);
+		}
+	}
+}
 
-	def reproduce(selected, pop_size, p_cross, p_mutation)
-	children = []
-	selected.each_with_index do | p1, i |
-	p2 = (i.modulo(2) == 0) ? selected[i + 1] : selected[i - 1]
-	p2 = selected[0] if i == selected.size - 1
-	child = {}
-	child[:bitstring] = crossover(p1[:bitstring], p2[:bitstring], p_cross)
-	child[:bitstring] = point_mutation(child[:bitstring], p_mutation)
-	children << child
-	break if children.size >= pop_size
-	end
-	return children
-	end
+def reproduce(selected, pop_size, p_cross, p_mutation)
+children = []
+selected.each_with_index do | p1, i |
+p2 = (i.modulo(2) == 0) ? selected[i + 1] : selected[i - 1]
+p2 = selected[0] if i == selected.size - 1
+child = {}
+child[:bitstring] = crossover(p1[:bitstring], p2[:bitstring], p_cross)
+child[:bitstring] = point_mutation(child[:bitstring], p_mutation)
+children << child
+break if children.size >= pop_size
+end
+return children
+end
 
 double search(const int max_gens, const int num_bits, const int pop_size, const double p_crossover, const double p_mutation)
 {
 	std::vector<std::string> population_part;
 	std::vector<std::vector<std::string>> population_bitstring;
 	std::vector<int> population_fitness;
-	std::map<std::string,int> population_map;
+	std::map<std::string, int> population_map;
 
-	
+
 	for (i = 0; i < pop_size; i++) {
 		random_bitstring(num_bits, population_part);
 		population_bitstring.push_back(population_part);
 	}
 
-	
-	for(auto member : population_bitstring){
+
+	for (auto member : population_bitstring) {
 		int sum_of_vec = onemax(member);
 		population_map[member] = sum_of_vec;
-		population_fitness.push_back(sum_of_vec);	
+		population_fitness.push_back(sum_of_vec);
 	}
-	
-		best = population.sort{ | x,y | y[:fitness] <= > x[:fitness] }.first
+
+	best = population.sort{ | x,y | y[:fitness] <= > x[:fitness] }.first
 		for (int i = 0; i<) {
 			selected = Array.new(pop_size){ | i | binary_tournament(population_fitness) }
 				children = reproduce(selected, pop_size, p_crossover, p_mutation)
@@ -106,12 +129,12 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 				break if best[:fitness] == num_bits
 				end
 				return best
-}
+		}
 
-int main
-{
-	//problem configuration
-	const int num_bits = 64;
+	int main
+	{
+		//problem configuration
+		const int num_bits = 64;
 	//algorithm configuration
 	const int max_gens = 100;
 	const int pop_size = 100;
@@ -120,4 +143,4 @@ int main
 	//execute the algorithm
 	double best = search(max_gens, num_bits, pop_size, p_crossover, p_mutation);
 	std::cout << "done! Solution: f= " << ; //#{best[:fitness]}, s=#{best[:bitstring]}"
-}
+	}
