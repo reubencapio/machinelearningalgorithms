808f0b7b031326cd24aaf80ba7aef6a51175f925
reuben capio <reubencapio@gmail.com>
2016-05-12 16:06:25 +0800

    Update evolutionary_genetic_algorithm.cpp

--- evolutionary_genetic_algorithm.cpp
+++ evolutionary_genetic_algorithm.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <algorithm>
 
+//create and return a random number between min and max inputs
 double createRandom(const double min, const double max)
 {
 	double result = 0;
@@ -16,6 +17,8 @@ double createRandom(const double min, const double max)
 	return result;
 }
 
+//count all "1"s in a string vector
+//return sum
 int onemax(std::vector<std::string> &bitstring)
 {
 	int sum = 0;
@@ -27,6 +30,7 @@ int onemax(std::vector<std::string> &bitstring)
 	return sum;
 }
 
+//create a string vector fille with random "1"s and "0"s
 void random_bitstring(int num_bits, std::vector<std::string> &bitstring)
 {
 	const double min = 0.000000000000000001;
@@ -36,7 +40,8 @@ void random_bitstring(int num_bits, std::vector<std::string> &bitstring)
 	}
 }
 
-
+//access 2 random places from a vector(i and j), if "i" is bigger, 
+//return bistring vector member "i" else return "j"
 std::vector<std::string> binary_tournament(const std::vector<int> &population_fitness, const std::vector<std::vector<std::string>> population_bitstring)
 {
 	std::random_device rd;
@@ -54,6 +59,8 @@ std::vector<std::string> binary_tournament(const std::vector<int> &population_fi
 	return (population_fitness[i] > population_fitness[j]) ? population_bitstring[i] : population_bitstring[j];
 }
 
+//create a child string vector based on random value generator and input bitstring
+//this serves as mutation function
 void point_mutation(const std::vector<std::string> &bitstring, std::vector<std::string> &child, double rate)
 {
 	//double rate = 1 / bitstring.size();
@@ -65,6 +72,8 @@ void point_mutation(const std::vector<std::string> &bitstring, std::vector<std::
 	}
 }
 
+//return a vector of string created from a mix of both parent string vectors if random is lesser than rate
+//else return parent1 input vector
 void crossover(const std::vector<std::string> &parent1, const std::vector<std::string> &parent2, const double rate, std::vector<std::string> &child_bitstring)
 {
 	const double min = 0.000000000000000001;
@@ -110,8 +119,10 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 	std::vector<std::vector<std::string>> population_bitstring;
 	std::vector<int> population_fitness;
 	std::map<int, std::vector<std::string>> population_map;
-	std::map<int, std::vector<std::string>> best;
+	std::map<int, std::vector<std::string>> best_map;
 	std::vector<std::vector<std::string>> sorted_best_bitstring;
+
+	//create random bitstring and push them in population_bitstring vector
 	for (int i = 0; i < pop_size; i++) {
 		random_bitstring(num_bits, population_part);
 		population_bitstring.push_back(population_part);
@@ -119,6 +130,8 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 
 	int max_sum_of_vec = 0;
 	std::vector<std::string> best_string;
+	//for each population_bitstring vector member, sum the bitstring members
+	//save the member with the highest sum as best
 	for (auto member : population_bitstring) {
 		int sum_of_vec = onemax(member);
 		if (sum_of_vec > max_sum_of_vec) {
@@ -128,7 +141,7 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 		population_map[sum_of_vec] = member;
 		population_fitness.push_back(sum_of_vec);
 	}
-	best[max_sum_of_vec] = best_string;
+	best_map[max_sum_of_vec] = best_string;
 
 	std::sort(population_fitness.rbegin(), population_fitness.rend());   // descending sort
 	for (auto val : population_fitness) {
@@ -148,7 +161,7 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 		std::vector<int> children_fitness;
 		std::map<int, std::vector<std::string>> children_map;
 		reproduce(selected, pop_size, p_crossover, p_mutation, children_bitstring);
-		
+
 
 		for (auto child : children_bitstring) {
 			int child_fitness = onemax(child);
@@ -157,7 +170,7 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 				best_string = child;
 			}
 			children_fitness.push_back(onemax(child));
-			best[child_fitness] = child;
+			best_map[child_fitness] = child;
 		}
 		//TODO: sort children to get biggest value then compare to current best
 
@@ -165,12 +178,12 @@ double search(const int max_gens, const int num_bits, const int pop_size, const 
 
 
 		for (auto val : children_fitness) {
-			std::vector<std::string> child_string = best[val];
+			std::vector<std::string> child_string = best_map[val];
 			sorted_children_bitstring.push_back(child_string);
 		}
 
-		if (children_fitness[0] > population_fitness[0]) {
-
+		if (children_fitness[0] > best_string) {
+			best_string = sorted_children_bitstring[0];
 		}
 
 		best = children.first if children.first[:fitness] >= best[:fitness]
